0d65a736c9e52a358a37ed321fcf0b98f174b0f2
D. Lisin <mrlisdim@gmail.com>
2019-09-23 00:11:34 +0300

    usb: Add MSC SCSI VPD 80h and 00h pages support

--- include/libopencm3/usb/msc.h
+++ include/libopencm3/usb/msc.h
@@ -84,6 +84,7 @@ usbd_mass_storage *usb_msc_init(usbd_device *usbd_dev,
 				 const char *vendor_id,
 				 const char *product_id,
 				 const char *product_revision_level,
+				 const char *product_serial,
 				 const uint32_t block_count,
 				 int (*read_block)(uint32_t lba, uint8_t *copy_to),
 				 int (*write_block)(uint32_t lba, const uint8_t *copy_from));
